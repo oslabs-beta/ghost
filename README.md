6039ceb758425f9fc84d71166cedbf150eb76e8d
Lisa Tian <102191527+lisatiann@users.noreply.github.com>
2022-10-17 19:28:04 -0700

    electron setup default

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OSP-AWS-Lambda-Metrics
+
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/electron.ts',
+    target: 'electron-main',
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'electron.js'
+    }
+  },
+  {
+    mode: 'development',
+    entry: './src/react.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'react.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }
+];
